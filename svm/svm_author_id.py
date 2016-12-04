b13ef9f01c027d2ebea12278545b7849ac9d6065
Jeremy Klein <jlklein@google.com>
2016-12-04 12:22:13 -0800

    finished svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,27 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+print 'Creating'
+#clf = svm.SVC(kernel='linear', cache_size=1000)
+clf = svm.SVC(kernel='rbf', cache_size=1000, C=10000)
+print 'Fitting...'
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+print 'Predicting...'
+t1 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t1, 3), "s"
+
+print accuracy_score(labels_test, pred)
+print sum(pred)
 
 #########################################################
 
